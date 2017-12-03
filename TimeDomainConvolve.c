421644183d23f13c12d6ea4fea7515d9fad4a208
Daniel D <cloudcrypt@users.noreply.github.com>
2017-12-03 16:17:09 -0700

    Added convolve. Currently not working due to static-ness of parsing structs

--- TimeDomainConvolve.c
+++ TimeDomainConvolve.c
@@ -19,10 +19,17 @@ typedef struct FmtChunk {
 
 typedef struct DataChunk {
     char chunkId[4];
-    int chunkSize;
+    uint32_t chunkSize;
     int16_t data[];
     //int16_t *data;
 } DataChunk;
+
+typedef struct DataChunk2 {
+    char chunkId[6];
+    uint32_t chunkSize;
+    int16_t data[];
+    //int16_t *data;
+} DataChunk2;
 
 typedef struct WavHeader {
     char chunkId[4];
@@ -36,6 +43,13 @@ typedef struct WavFile {
     DataChunk dataChunk;
 } WavFile;
 
+typedef struct WavFile2 {
+    WavHeader header;
+    //char padding[1];
+    DataChunk2 dataChunk;
+} WavFile2;
+
+void convolve(float x[], int N, float h[], int M, float y[], int P);
 void scaleSamples(int16_t samples[], int numSamples, float scaled[]);
 void unscaleSamples(float scaled[], int numSamples, int16_t samples[]);
 void createWavFile(char* fileName, WavFile *wavFile);
@@ -71,24 +85,76 @@ int main(int argc, char *argv[]) {
     WavFile *inputWav = malloc(size);
     fseek(inputFile, 0, SEEK_SET);
     fread(inputWav, 1, size, inputFile);
+    fclose(inputFile);
 
-    int numSamples = inputWav->dataChunk.chunkSize / (BITS_PER_SAMPLE / 8);
-    float *x = malloc(sizeof(float) * numSamples);
-    scaleSamples(inputWav->dataChunk.data, numSamples, x);
+    FILE *impulseFile = fopen(IRfileName, "rb");
+    fseek(impulseFile, 0, SEEK_END);
+    size = ftell(impulseFile);
+    WavFile2 *impulseWav = malloc(size);
+    fseek(impulseFile, 0, SEEK_SET);
+    fread(impulseWav, 1, size, impulseFile);
+    fclose(impulseFile);
+
+    int inputSamples = inputWav->dataChunk.chunkSize / (BITS_PER_SAMPLE / 8);
+    float *x = malloc(sizeof(float) * inputSamples);
+    scaleSamples(inputWav->dataChunk.data, inputSamples, x);
+
+    int impulseSamples = impulseWav->dataChunk.chunkSize / (BITS_PER_SAMPLE / 8);
+    float *h = malloc(sizeof(float) * impulseSamples);
+    scaleSamples(impulseWav->dataChunk.data, impulseSamples, h);
+
+    int convolvedSamples = inputSamples + impulseSamples - 1;
+    float *y = malloc(sizeof(float) * convolvedSamples);
+
+    convolve(x, inputSamples, h, impulseSamples, y, convolvedSamples);
     // for (int i = 0; i < numSamples; i++) {
     //     *(x + i) = *(x + i) / (float)2;
     // }
-    unscaleSamples(x, numSamples, inputWav->dataChunk.data);
+
+    int addedBytes = (convolvedSamples - inputSamples) * (BITS_PER_SAMPLE / 8);
+    inputWav->header.chunkSize += addedBytes;
+    inputWav = realloc(inputWav, inputWav->header.chunkSize + 8);
+    inputWav->dataChunk.chunkSize += addedBytes;
+    // for (int i = numSamples; i < (numSamples + 3); i++) {
+    //     *(inputWav->dataChunk.data + i) = 0xbabe;
+    // }
+    unscaleSamples(y, convolvedSamples, inputWav->dataChunk.data);
 
     createWavFile(outputFileName, inputWav);
 
     return 0;
 }
 
+void convolve(float x[], int N, float h[], int M, float y[], int P) {
+    int n, m;
+
+    /*  Make sure the output buffer is the right size: P = N + M - 1  */
+    if (P != (N + M - 1)) {
+        printf("Output signal vector is the wrong size\n");
+        printf("It is %-d, but should be %-d\n", P, (N + M - 1));
+        printf("Aborting convolution\n");
+        return;
+    }
+
+    /*  Clear the output buffer y[] to all zero values  */  
+    for (n = 0; n < P; n++)
+        y[n] = 0.0;
+
+    /*  Do the convolution  */
+    /*  Outer loop:  process each input value x[n] in turn  */
+    int totalIterations = N * M;
+    for (n = 0; n < N; n++) {
+        /*  Inner loop:  process x[n] with each sample of h[]  */
+        for (m = 0; m < M; m++)
+            printf("Processing %d/%d...", (n * m) + m, totalIterations);
+            y[n+m] += x[n] * h[m];
+    }
+}
+
 void scaleSamples(int16_t samples[], int numSamples, float scaled[]) {
     for (int i = 0; i < numSamples; i++) {
         int16_t sample = *(samples + i);
-        float test = sample / (float)(maxValue + (sample < 0 ? 1 : 0));
+        //float test = sample / (float)(maxValue + (sample < 0 ? 1 : 0));
         *(scaled + i) = sample / (float)(maxValue + (sample < 0 ? 1 : 0));
     }
 }
@@ -96,8 +162,8 @@ void scaleSamples(int16_t samples[], int numSamples, float scaled[]) {
 void unscaleSamples(float scaled[], int numSamples, int16_t samples[]) {
     for (int i = 0; i < numSamples; i++) {
         float scaledSample = *(scaled + i);
-        int16_t test = (int16_t)rintf(scaledSample * maxValue);
-        *(samples + i) = (int16_t)rintf(scaledSample * maxValue);
+        //int16_t test = (int16_t)rintf(scaledSample * maxValue);
+        *(samples + i) = (int16_t)rintf(scaledSample * (maxValue + (scaledSample < 0 ? 1 : 0)));
     }
 }
 
