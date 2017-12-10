ed9b9db0380d120cb02aa9cfede81dd61d8bf69b
Daniel D <cloudcrypt@users.noreply.github.com>
2017-12-10 16:43:41 -0700

    Performed optimization of minimizing array references on FFTConvolve

--- FrequencyDomainConvolve.c
+++ FrequencyDomainConvolve.c
@@ -92,8 +92,12 @@ int main(int argc, char *argv[]) {
 
     double *f = calloc(neededLength, sizeof(double));
     for (int i = 0; i < neededLength; i+=2) {
-        *(f + i) = (*(x + i) * *(h + i)) - (*(x + i + 1) * *(h + i + 1));
-        *(f + i + 1) = (*(x + i + 1) * *(h + i)) + (*(x + i) * *(h + i + 1));
+        double realX = *(x + i);
+        double realH = *(h + i);
+        double imX = *(x + i + 1);
+        double imH = *(h + i + 1);
+        *(f + i) = (realX * realH) - (imX * imH);
+        *(f + i + 1) = (imX * realH) + (realX * imH);
     }
     four1(f - 1, neededLength / 2, -1);
 
